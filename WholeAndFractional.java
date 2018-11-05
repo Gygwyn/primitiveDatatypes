aa8cd85699ba148c96a227348eb285fdcc303981
Gygwyn <42860996+Gygwyn@users.noreply.github.com>
2018-11-05 11:45:04 +0100

    Add files via upload

---
+++ WholeAndFractional.java
@@ -0,0 +1,14 @@
+package primitiveDatatypes;
+
+
+public class WholeAndFractional {
+
+	
+	public static void main(String[] args) {
+		
+		double res = Math.nextDown(4.5 / 3.1);
+		System.out.println("Result: " + res);
+		
+	}
+	
+}
