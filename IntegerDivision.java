76b6d8c0e946c06eace5f3667e4aeaff5184d43e
Gygwyn <42860996+Gygwyn@users.noreply.github.com>
2018-11-05 11:27:46 +0100

    Add files via upload

---
+++ IntegerDivision.java
@@ -0,0 +1,13 @@
+package primitiveDatatypes;
+
+public class IntegerDivision {
+
+	public static void main(String[] args) {
+		
+		int tot_theory_marks = 350, tot_practical_marks = 90, tot_marks = 500;
+		int procent_av_marks = ((tot_theory_marks + tot_practical_marks)*100)/tot_marks;
+			System.out.print("\nProcent av marks: " + procent_av_marks+"%\n");
+
+	}
+
+}
